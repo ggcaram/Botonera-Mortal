916968a7d65b3c752c06dfa5bcc188143c6423cb
Gabriel Guillermo Cura Caram <63132435+ggcaram@users.noreply.github.com>
2021-07-04 12:32:29 -0300

    Update index.js
    Se comentan algunas lineas del codigo

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import "./styles.css";
 window.addEventListener('keydown', playSound);
 
 function playSound(e) {
+  //Permite que el sistema funcione con block mayus activado
   const key = e.key.toLowerCase() ;
+  //Toma la info de la tecla presionada y el audio asignado a la misma
   const mySound = document.querySelector(`audio[data-key="${key}"]`);
+  //Relaciona el boton con la tecla presionada
   const pad = document.querySelector(`.pad[data-key="${key}"]`);
   
   if (!mySound) return;
@@ -13,6 +16,7 @@ function playSound(e) {
   mySound.play();
   pad.classList.add('playing');
   
+  // Para el sonido despes de 100 milisegundos
   setTimeout(() => {
     pad.classList.remove('playing');
   }, 100)
